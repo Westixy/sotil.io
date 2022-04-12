f073a7cfb1afbff8a1794431b156be98bc00ac70
Esteban Sotillo <esteban.sotillo@nexthink.com>
2022-04-12 17:57:24 +0200

    update cv

--- src/App.svelte
+++ src/App.svelte
@@ -5,18 +5,20 @@
 <main>
 	<TypeWriter delay="0">
 		<pre><code>.
-           _   _ _   _       
-          | | (_) | (_)      
- ___  ___ | |_ _| |  _  ___  
-/ __|/ _ \| __| | | | |/ _ \ 
+           _   _ _   _
+          | | (_) | (_)
+ ___  ___ | |_ _| |  _  ___
+/ __|/ _ \| __| | | | |/ _ \
 \__ \ (_) | |_| | |_| | (_) |
-|___/\___/ \__|_|_(_)_|\___/ 
+|___/\___/ \__|_|_(_)_|\___/
+
+Esteban Sotillo
 	</code></pre>
 		<br />
 		Welcome to my website. Nothing much to see but you can get some links: <br /><br />
 		- <a href="https://www.linkedin.com/in/esteban-sotillo-750893133/">Linkedin profile</a><br />
 		- <a href="https://github.com/Westixy">GitHub account</a><br />
-		- <a href="https://mega.nz/file/BI8VBCjL#_mAKQf2bfl41-c1vI_KojzWObSE25h3WF1DlT7T-3F8">Current CV</a><br />
+		- <a href="https://github.com/Westixy/cv/raw/master/cv_2022.pdf">Current CV</a><br />
 		-
 		<a href="https://keys.openpgp.org/vks/v1/by-fingerprint/7F4CA6F85707C506D5FFADEE7999EDD80EFE6A8E"
 			>PGP public key</a
